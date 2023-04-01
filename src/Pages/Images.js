3b861d4314f10af151bf41abd837bc155c6bde02
Mike Stevens <mike@mikestevens.design>
2023-04-01 14:22:56 -0400

    fetch images and render images from api

--- src/Pages/Images.js
+++ src/Pages/Images.js
@@ -1,5 +1,26 @@
+import { useState, useEffect } from "react";
+
 const Images = () => {
-  return <h1>Images</h1>;
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    async function fetchImages() {
+      const response = await fetch(
+        "http://127.0.0.1:5001/mikestevensphoto-c810c/us-central1/app/images"
+      );
+      const data = await response.json();
+      setImages(data);
+    }
+    fetchImages();
+  }, []);
+
+  return (
+    <div>
+      {images.map((image) => (
+        <img src={image.url} alt={image.metadata.name} />
+      ))}
+    </div>
+  );
 };
 
 export default Images;
