ebc00df2efe8718fb69269b0f5b307562a3f6bb7
Mike Stevens <mike@mikestevens.design>
2023-04-01 14:25:36 -0400

    add events fetch and render from api

--- src/Pages/Events.js
+++ src/Pages/Events.js
@@ -1,5 +1,28 @@
+import { useState, useEffect } from "react";
+
 const Events = () => {
-  return <h1>Events</h1>;
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5001/mikestevensphoto-c810c/us-central1/app/events")
+      .then((response) => response.json())
+      .then((data) => setEvents(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Events</h1>
+      <ul>
+        {events.map((event) => (
+          <li key={event.id}>
+            <p>{event.name}</p>
+            {event.desc && <p>{event.desc}</p>}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Events;
