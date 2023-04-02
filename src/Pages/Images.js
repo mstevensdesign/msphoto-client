d9d6052901393dff0b678753e58a12f086f032ac
Mike Stevens <mike@mikestevens.design>
2023-04-01 20:12:22 -0400

    add image modal

--- src/Pages/Images.js
+++ src/Pages/Images.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
-import { Container, Image } from "react-bootstrap";
+import { Container, Image, Modal, Form, Button } from "react-bootstrap";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 const Images = () => {
   const [images, setImages] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     async function fetchImages() {
@@ -16,14 +19,73 @@ const Images = () => {
     fetchImages();
   }, []);
 
+  const handleOpenModal = (image) => {
+    setSelectedImage(image);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+    setShowModal(false);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSendEmail = () => {
+    alert(`Email ${email} sent for image ${selectedImage.metadata.name}`);
+    handleCloseModal();
+  };
+
   return (
-    <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
-      <Masonry>
-        {images.map((image) => (
-          <Image fluid src={image.url} alt={image.metadata.name} />
-        ))}
-      </Masonry>
-    </ResponsiveMasonry>
+    <>
+      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+        <Masonry>
+          {images.map((image) => (
+            <div
+              key={image.metadata.name}
+              onClick={() => handleOpenModal(image)}
+            >
+              <Image fluid src={image.url} alt={image.metadata.name} />
+            </div>
+          ))}
+        </Masonry>
+      </ResponsiveMasonry>
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Send Email for {selectedImage?.metadata?.name}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Image src={selectedImage?.url} thumbnail />
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={handleEmailChange}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSendEmail}>
+            Send
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
